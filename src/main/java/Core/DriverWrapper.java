0b7f77566bed46b0f3a4967ded8882b4150cc7c5
Andrew Valkovski <andriy.valkovski@gmail.com>
2014-10-28 11:38:38 +0200

    fixing comments.

--- src/main/java/Core/DriverWrapper.java
+++ src/main/java/Core/DriverWrapper.java
@@ -10,14 +10,6 @@ import Utils.Propertiator;
 public class DriverWrapper {
     private static WebDriver driver = null;
 
-//    private DriverWrapper() {
-//        EventFiringWebDriver eDriver  = new EventFiringWebDriver(new ChromeDriver());
-//        eDriver.register(new MyEventListener());
-//        driver = eDriver;
-//        driver.manage().window().maximize();
-//    }
-
-//    private DriverWrapper(DriverType type) {
     private DriverWrapper() {
         EventFiringWebDriver eDriver = null;
         String type = Propertiator.getPropertie("driver");
@@ -34,15 +26,6 @@ public class DriverWrapper {
       driver = eDriver;
       driver.manage().window().maximize();
   }
-
-//    public static WebDriver getDriver(DriverType type) {
-//        if(driver != null) {
-//            return driver;
-//        } else {
-//            new DriverWrapper(type);
-//            return driver;
-//        }
-//    }
 
     public static WebDriver getDriver() {
         if(driver != null) {
