c002c4b368cac6effea694422650e6c490aaa7e9
Andrew Valkovski <andriy.valkovski@gmail.com>
2015-03-04 15:59:35 +0200

    fixes

--- src/main/java/core/DriverWrapper.java
+++ src/main/java/core/DriverWrapper.java
@@ -1,5 +1,10 @@
 package core;
 
+import io.selendroid.client.SelendroidDriver;
+import io.selendroid.common.SelendroidCapabilities;
+import io.selendroid.standalone.SelendroidConfiguration;
+import io.selendroid.standalone.SelendroidLauncher;
+
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
@@ -17,6 +22,7 @@ public class DriverWrapper {
 
     private DriverWrapper() {
         EventFiringWebDriver eDriver = null;
+        SelendroidDriver sDriver = null;
         String type = Propertiator.getPropertie("driver");
         switch(type) {
         case "Chrome":
@@ -25,10 +31,28 @@ public class DriverWrapper {
         case "Firefox":
             eDriver  = new EventFiringWebDriver(new FirefoxDriver());
             break;
+        case "Selen":
+            SelendroidConfiguration config = new SelendroidConfiguration();
+            config.addSupportedApp("selendroid-test-app-0.15.0.apk");
+            SelendroidLauncher selendroidServer = new SelendroidLauncher(config);
+            selendroidServer.launchSelendroid();
+            SelendroidCapabilities caps = new SelendroidCapabilities("io.selendroid.testapp:0.15.0");
+            try {
+                sDriver = new SelendroidDriver(caps);
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            break;
      }
       eDriver.register(new MyEventListener());
-      driver = eDriver;
-      driver.manage().window().maximize();
+      if(driver!=null){
+          driver = eDriver;
+          driver.manage().window().maximize();
+      }
+      else {
+          driver = sDriver;
+      }
   }
 
     public static WebDriver getDriver() {
